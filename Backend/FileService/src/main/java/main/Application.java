fe7c22df31fe0be692ff1bf579f5226a18d877b7
Pankamol Srikaew <aim.ske09@gmail.com>
2016-01-30 02:17:52 +0900

    try @POST in upload file

--- Backend/FileService/src/main/java/main/Application.java
+++ Backend/FileService/src/main/java/main/Application.java
@@ -3,6 +3,8 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import mongodb.main.MongoDBMain;
+
 
 //import mongodb.main.MongoDBMain;
 
@@ -10,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Starting File Service ...");
-//		MongoDBMain.run();
+		MongoDBMain.run();
 		System.out.println("Starting Spring Application...");
 		SpringApplication.run(Application.class, args);
 	}
--- Backend/FileService/src/main/java/main/rest/FileRest.java
+++ Backend/FileService/src/main/java/main/rest/FileRest.java
@@ -1,5 +1,9 @@
 package main.rest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,15 +15,30 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
 import org.springframework.web.client.RestTemplate;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.gridfs.GridFSDBFile;
+
 import main.model.File;
+import mongodb.main.SpringMongoConfig;
 
 
 @Named
 @Path("/")
 public class FileRest {
 	private List<File> files = new ArrayList<File>();
+	
+	AnnotationConfigApplicationContext ctx = 
+            new AnnotationConfigApplicationContext(SpringMongoConfig.class);
+GridFsOperations gridOperations = 
+             (GridFsOperations) ctx.getBean("gridFsTemplate");
+
 	
 	@Inject
 	private RestTemplate restTemplate;
@@ -30,5 +49,56 @@ public class FileRest {
 	public List<File> getReviewByDocumentId() {
 		//TODO DAO to get all file
 		return files;
+	}
+	
+	@GET
+	@Path("upfile")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String uploadFile() {
+		InputStream inputStream = null;
+		DBObject metaData = new BasicDBObject();
+		metaData.put("extra1", "anything 1");
+		metaData.put("extra2", "anything 2");
+		try {
+			inputStream = new FileInputStream("/Users/AimeTPGM/Desktop/testing.png");
+			gridOperations.store(inputStream, "testing.png", "image/png", metaData);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+			System.out.println("Done");
+		return "uploaded";
+	}
+	
+	@GET
+	@Path("downloadfile")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String downloadFile() {
+		List<GridFSDBFile> result = gridOperations.find(
+	               new Query().addCriteria(Criteria.where("filename").is("testing.png")));
+
+		for (GridFSDBFile file : result) {
+			try {
+				System.out.println(file.getFilename());
+				System.out.println(file.getContentType());
+					
+				//save as another image
+				file.writeTo("/Users/AimeTPGM/Downloads/new-testing.png");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("Done");
+		return "downloaded";
 	}
 }
