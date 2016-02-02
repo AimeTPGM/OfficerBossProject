b9e522550853d360bd06d57d30b2121899055056
Pankamol Srikaew <aim.ske09@gmail.com>
2016-02-03 00:55:21 +0900

    add draft of dashboard

--- Frontend/www/js/app.js
+++ Frontend/www/js/app.js
@@ -102,6 +102,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
+    .state('app.dashboard', {
+      url: '/dashboard',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/dashboard.html'
+         
+
+        }
+      }
+    })
 ;
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/login');
