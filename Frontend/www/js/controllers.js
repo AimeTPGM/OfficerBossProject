3aff1401d37f27f92a253a4f66272a0bd5de2fc2
Pankamol Srikaew <aim.ske09@gmail.com>
2016-02-06 22:25:16 +0900

    update test add file with documentId

--- Frontend/www/js/controllers.js
+++ Frontend/www/js/controllers.js
@@ -93,8 +93,9 @@ angular.module('starter.controllers', ['ngFileUpload'])
         Upload.upload({
             url: 'http://localhost:8084/upload',
             method: 'POST',
-            data: {file: file}
+            data: {file: file, documentId: "0"}
         }).then(function (resp) {
+            
             console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
         }, function (resp) {
             console.log('Error status: ' + resp.status);
