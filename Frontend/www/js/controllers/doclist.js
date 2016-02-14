f374723e6e09c2be6ca84836ad1be00aa696f1f4
Pankamol Srikaew <aim.ske09@gmail.com>
2016-02-14 18:17:47 +0900

    integrating folder.documentIdList with document service

--- Frontend/www/js/controllers/doclist.js
+++ Frontend/www/js/controllers/doclist.js
@@ -4,11 +4,25 @@ angular.module('starter.controllers')
     disableBack: true
   });
   $scope.documents = {};
-  console.log($stateParams)
   $http.get('http://localhost:8085/folder?folderId='+$stateParams.folderId)
   .success(function(data){
     console.log(data)
     $scope.folder = data;
+    if($scope.folder.numberOfDocuments > 0){
+      for (var i = $scope.folder.numberOfDocuments - 1; i >= 0; i--) {
+        $http.get('http://localhost:8081/getdocument?documentid='+$scope.folder.documentIdList[i])
+          .success(function(data){
+            $scope.folder.documentIdList[i] = data;
+          })
+          .error(function(data){
+            console.log('cannot reach user-service port 8082')
+          });
+      };
+      console.log($scope.folder)
+    }
+    else{
+      console.log('no document')
+    }
     
   })
   .error(function(data){
@@ -20,13 +34,13 @@ angular.module('starter.controllers')
   // $http.get('http://localhost:8081/getalldocumentsbyuserid?userid=1')
   // .success(function(data){
   //   $scope.documents = data;
-  //   $http.get('http://localhost:8082/getuser?userid=1')
-  //   .success(function(data){
-  //     $scope.user = data;
-  //   })
-  //   .error(function(data){
-  //     console.log('cannot reach user-service port 8082')
-  //   });
+    // $http.get('http://localhost:8082/getuser?userid=1')
+    // .success(function(data){
+    //   $scope.user = data;
+    // })
+    // .error(function(data){
+    //   console.log('cannot reach user-service port 8082')
+    // });
   // })
 
 
