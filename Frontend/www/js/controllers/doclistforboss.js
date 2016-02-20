624000ec8349024b25eacbb340f360b19dea92ba
Pankamol Srikaew <aim.ske09@gmail.com>
2016-02-21 04:57:05 +0900

    fixed defects

--- Frontend/www/js/controllers/doclistforboss.js
+++ Frontend/www/js/controllers/doclistforboss.js
@@ -8,7 +8,7 @@ angular.module('starter.controllers')
     .success(function(data){
       $scope.documents = data;
       var alldoc = data;
-
+      var temp = {};
       $http.get('http://localhost:8082/getusers')
         .success(function(data){
           var temp_users = data;
@@ -17,10 +17,29 @@ angular.module('starter.controllers')
                 if (alldoc[i].creatorId == temp_users[j].userId){
                   $scope.documents[i].creatorName = temp_users[j].lastname+" "+temp_users[j].firstname;
                   $scope.documents[i].lastDocId = $scope.documents[i].documentList[($scope.documents[i].documentList.length)-1];
-                  console.log($scope.documents[i]);
+                  temp[i] = $scope.documents[i].lastDocId;
                   break;
                 }
               };
+          };
+
+          console.log(temp)
+          for (var i = 0; i < alldoc.length; i++) {
+            console.log(temp[i])
+                $http.get('http://localhost:8081/getdocument?documentid='+temp[i])
+                  .success(function(data){
+                    for (var j = 0; j < alldoc.length; j++) {
+                      if(temp[j] == data.documentId){
+                        $scope.documents[j].lastDocData = data;
+                        console.log($scope.documents[j].lastDocData)
+                        break;
+                      }
+                    };
+                  })
+                  .error(function(data){
+                    console.log('cannot reach document-service port 8081')
+                  });
+
           };
 
           
@@ -29,6 +48,10 @@ angular.module('starter.controllers')
         .error(function(data){
           console.log('cannot reach user-service port 8082')
         });
+
+
+
+      
 
         
     })
