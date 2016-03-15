eeb9b80f0a83e3ba8a81d31fc54205651930f6f8
Pankamol Srikaew <aim.ske09@gmail.com>
2016-03-15 14:05:56 +0900

    register form validation

--- Frontend/www/js/controllers/register.js
+++ Frontend/www/js/controllers/register.js
@@ -6,7 +6,31 @@ angular.module('starter.controllers')
 
     $scope.user = {};
     console.log($scope.user)
-
+    $scope.emailCheck = function(){
+      var emailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(emailFormat.test($scope.user.email)){
+        $scope.emailCorrect = function(){
+          return true;
+        }  
+      }
+      else{
+        $scope.emailCorrect = function(){
+          return false;
+        }
+      }
+    }
+    $scope.passwordCheck = function(){
+      if($scope.user.password == $scope.user.confirmpw){
+        $scope.passwordMatch = function(){
+          return true;
+        }
+      }
+      else{
+        $scope.passwordMatch = function(){
+          return false;
+        }
+      }
+    }
     $scope.register = function(){
 
       $http({
@@ -15,7 +39,7 @@ angular.module('starter.controllers')
         headers: {'Content-Type': 'application/json'},
         data: $scope.user
     
-    })
+      })
       .success(function(data, status, headers, config) {
         console.log('sent POST request: successfully create new officer');
         console.log(data);
