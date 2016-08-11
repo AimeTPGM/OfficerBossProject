d14938eeb1d76fa66c755a32165083c736efcefa
Pankamol Srikaew <aim.ske09@gmail.com>
2016-08-11 18:58:54 +0900

    complete /documentDetail

--- MiddleLayer/index.js
+++ MiddleLayer/index.js
@@ -23,45 +23,66 @@ app.listen(port, function() {
 
 
 
-/* Routing */
+/* TEST SERVER */
 app.get('/', function (req, res) {
-    res.send('<h1>Hello Node.js</h1>');
+    res.send('<h1>Hello Node.js TEST</h1>');
 });
 
 /* officer */
 /*
  * get document detail 
- * params: 
+ * @params: 
  *		folderId
  *		documentId
- */ 
-
- /* finalResponse = [Document, Creator, ApproverListId, ApproverList, ]
+ * @return:
+ * 		finalResponse = [Document, Creator, ApproverListId, File, Folder]
  */
-app.get('/documentDetail/:docId', function (req, res) {
-	console.log('GET REQUEST: get all users from user service')
+app.get('/documentDetail/:folderId/:docId', function (req, res) {
+	console.log('=================================')
+	console.log('GET REQUEST: get document detail')
+	console.log('=================================')
 	var finalResponse = [];
 	var docId = req.params.docId;
+	var folderId = req.params.folderId;
 	/* Step 1 get document detail from docId */
 	requestify.get(documentServicePath+'getDocument?documentId='+docId).then(function(response) {
+    	console.log('✓ document detail')
     	finalResponse = response.getBody();
-
     	/* Step 2 get user information from creator */
     	var creatorId = response.getBody().creator;
     	requestify.get(userServicePath+'user?userId='+creatorId).then(function(response) {
+	    	console.log('✓ creator detail')
 	    	finalResponse.creator = response.getBody();
-	    	
+	    	finalResponse.creatorresponse.getBody() = response.getBody().firstname;
+	    	finalResponse.creator.lastname = response.getBody().lastname;
+	    	finalResponse.creator.name = response.getBody().name;
+	    	finalResponse.creator.email = response.getBody().email;
+	    	finalResponse.creator.userId = response.getBody().userId;
+
 	    	/* Step 3 get approver list from approverId */
 	    	requestify.get(approverListServicePath+'approverListByDocumentId?documentId='+docId).then(function(response) {
-	    		finalResponse.approverId = response.getBody();
+	    		console.log('✓ approver list id')
+	    		finalResponse.approverId = response.getBody().approverIdList;
 	    		
-	    		/* Step 4 get each approver detail from approver list service*/
-	    		for (var i = 0; i < res.getBody().length; i++) {
+	    		/* Step 4 get all file detail */
+	    		requestify.get(fileServicePath+'allFileDetail?documentId='+docId).then(function(response) {
+	    			console.log('✓ files')
+	    			finalResponse.files = {};
+	    			finalResponse.files = response.getBody();
 	    			
-	    		};
+	    			/* Step 5 get folder detail */
+	    			requestify.get(folderServicePath+'folder?folderId='+folderId).then(function(response){
+	    				console.log('✓ folder detail')
+	    				finalResponse.folder = {};
+	    				finalResponse.folder.folderId = folderId;
+	    				finalResponse.folder.documentList = response.getBody().documentList;
+	    				console.log('--- END REQUEST ---')
+	    				res.json(finalResponse)
 
+	    			})
+
+	    		});
 	    	});
-	    	
 		});
 	});
 });
