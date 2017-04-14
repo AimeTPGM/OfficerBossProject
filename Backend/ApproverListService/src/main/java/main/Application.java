066f49aeaaa12601d89ca4725bce3362bf3d948e
Pankamol Srikaew <aim.ske09@gmail.com>
2017-04-14 18:18:11 +0900

    docker image hahahahahahahahha

--- Backend/ApproverListService/src/main/java/main/Application.java
+++ Backend/ApproverListService/src/main/java/main/Application.java
@@ -8,7 +8,7 @@ import mongodb.main.MongoDBMain;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		System.out.println("Starting Folder Service ...");
+		System.out.println("Starting Approver List Service ...");
 		MongoDBMain.run();
 		System.out.println("Starting Spring Application...");
 		SpringApplication.run(Application.class, args);
